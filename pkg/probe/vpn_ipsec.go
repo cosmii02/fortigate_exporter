05fae28778b0b5767ed6595dad464b81a4f7374c
cosmii02 <cosmii02@cosmii02.com>
2025-07-31 11:46:33 +0300

    ipsec client monitoring

--- pkg/probe/vpn_ipsec.go
+++ pkg/probe/vpn_ipsec.go
@@ -25,6 +25,26 @@ func probeVPNIPSec(c http.FortiHTTP, meta *TargetMetadata) ([]prometheus.Metric,
 			"Total number of bytes received over the IPsec tunnel",
 			[]string{"vdom", "name", "p2serial", "parent"}, nil,
 		)
+		activeTunnels = prometheus.NewDesc(
+			"fortigate_ipsec_tunnels_up",
+			"Number of active IPsec tunnels",
+			[]string{"vdom"}, nil,
+		)
+		totalTunnels = prometheus.NewDesc(
+			"fortigate_ipsec_tunnels_total",
+			"Total number of configured IPsec tunnels",
+			[]string{"vdom"}, nil,
+		)
+		activeConnections = prometheus.NewDesc(
+			"fortigate_ipsec_connections_up",
+			"Number of active IPsec connections (proxy IDs)",
+			[]string{"vdom"}, nil,
+		)
+		totalConnections = prometheus.NewDesc(
+			"fortigate_ipsec_connections_total",
+			"Total number of configured IPsec connections (proxy IDs)",
+			[]string{"vdom"}, nil,
+		)
 	)
 
 	type proxyid struct {
@@ -50,6 +70,13 @@ func probeVPNIPSec(c http.FortiHTTP, meta *TargetMetadata) ([]prometheus.Metric,
 	}
 
 	m := []prometheus.Metric{}
+
+	// Maps to track statistics per VDOM
+	vdomActiveTunnels := make(map[string]int)
+	vdomTotalTunnels := make(map[string]int)
+	vdomActiveConnections := make(map[string]int)
+	vdomTotalConnections := make(map[string]int)
+
 	for _, v := range res {
 		for _, i := range v.Results {
 			/*
@@ -59,7 +86,21 @@ func probeVPNIPSec(c http.FortiHTTP, meta *TargetMetadata) ([]prometheus.Metric,
 			if i.Type == "dialup" {
 				continue
 			}
+
+			// Count tunnels
+			vdomTotalTunnels[v.VDOM]++
+
+			// Check if tunnel has any active proxy IDs
+			hasActiveProxy := false
 			for _, t := range i.ProxyID {
+				// Count connections
+				vdomTotalConnections[v.VDOM]++
+
+				if t.Status == "up" {
+					vdomActiveConnections[v.VDOM]++
+					hasActiveProxy = true
+				}
+
 				s := 0.0
 				if t.Status == "up" {
 					s = 1.0
@@ -68,7 +109,20 @@ func probeVPNIPSec(c http.FortiHTTP, meta *TargetMetadata) ([]prometheus.Metric,
 				m = append(m, prometheus.MustNewConstMetric(transmitted, prometheus.CounterValue, t.Outgoing, v.VDOM, t.Name, strconv.Itoa(t.P2serial), i.Name))
 				m = append(m, prometheus.MustNewConstMetric(received, prometheus.CounterValue, t.Incoming, v.VDOM, t.Name, strconv.Itoa(t.P2serial), i.Name))
 			}
+
+			// If tunnel has at least one active proxy ID, count it as active
+			if hasActiveProxy {
+				vdomActiveTunnels[v.VDOM]++
+			}
 		}
+	}
+
+	// Add VDOM-level statistics
+	for vdom := range vdomTotalTunnels {
+		m = append(m, prometheus.MustNewConstMetric(activeTunnels, prometheus.GaugeValue, float64(vdomActiveTunnels[vdom]), vdom))
+		m = append(m, prometheus.MustNewConstMetric(totalTunnels, prometheus.GaugeValue, float64(vdomTotalTunnels[vdom]), vdom))
+		m = append(m, prometheus.MustNewConstMetric(activeConnections, prometheus.GaugeValue, float64(vdomActiveConnections[vdom]), vdom))
+		m = append(m, prometheus.MustNewConstMetric(totalConnections, prometheus.GaugeValue, float64(vdomTotalConnections[vdom]), vdom))
 	}
 	return m, true
 }
--- pkg/probe/vpn_ipsec_test.go
+++ pkg/probe/vpn_ipsec_test.go
@@ -17,6 +17,12 @@ func TestVPNIPSec(t *testing.T) {
 	}
 
 	em := `
+	# HELP fortigate_ipsec_connections_total Total number of configured IPsec connections (proxy IDs)
+	# TYPE fortigate_ipsec_connections_total gauge
+	fortigate_ipsec_connections_total{vdom="root"} 2
+	# HELP fortigate_ipsec_connections_up Number of active IPsec connections (proxy IDs)
+	# TYPE fortigate_ipsec_connections_up gauge
+	fortigate_ipsec_connections_up{vdom="root"} 1
 	# HELP fortigate_ipsec_tunnel_receive_bytes_total Total number of bytes received over the IPsec tunnel
 	# TYPE fortigate_ipsec_tunnel_receive_bytes_total counter
 	fortigate_ipsec_tunnel_receive_bytes_total{name="tunnel_1-sub",p2serial="1",parent="tunnel_1",vdom="root"} 1.429824e+07
@@ -29,6 +35,12 @@ func TestVPNIPSec(t *testing.T) {
 	# TYPE fortigate_ipsec_tunnel_up gauge
 	fortigate_ipsec_tunnel_up{name="tunnel_1-sub",p2serial="1",parent="tunnel_1",vdom="root"} 1
 	fortigate_ipsec_tunnel_up{name="tunnel_1-sub",p2serial="12",parent="tunnel_1",vdom="root"} 0
+	# HELP fortigate_ipsec_tunnels_total Total number of configured IPsec tunnels
+	# TYPE fortigate_ipsec_tunnels_total gauge
+	fortigate_ipsec_tunnels_total{vdom="root"} 1
+	# HELP fortigate_ipsec_tunnels_up Number of active IPsec tunnels
+	# TYPE fortigate_ipsec_tunnels_up gauge
+	fortigate_ipsec_tunnels_up{vdom="root"} 1
 
 	`
 
@@ -46,6 +58,12 @@ func TestVPNIPSecWithCommonP2Names(t *testing.T) {
 	}
 
 	em := `
+	# HELP fortigate_ipsec_connections_total Total number of configured IPsec connections (proxy IDs)
+	# TYPE fortigate_ipsec_connections_total gauge
+	fortigate_ipsec_connections_total{vdom="root"} 6
+	# HELP fortigate_ipsec_connections_up Number of active IPsec connections (proxy IDs)
+	# TYPE fortigate_ipsec_connections_up gauge
+	fortigate_ipsec_connections_up{vdom="root"} 4
 	# HELP fortigate_ipsec_tunnel_receive_bytes_total Total number of bytes received over the IPsec tunnel
 	# TYPE fortigate_ipsec_tunnel_receive_bytes_total counter
 	fortigate_ipsec_tunnel_receive_bytes_total{name="CommonP2",p2serial="22",parent="My VPN",vdom="root"} 0
@@ -70,6 +88,12 @@ func TestVPNIPSecWithCommonP2Names(t *testing.T) {
 	fortigate_ipsec_tunnel_up{name="CommonP2",p2serial="25",parent="My VPN",vdom="root"} 1
 	fortigate_ipsec_tunnel_up{name="mgmt",p2serial="1",parent="My VPN",vdom="root"} 0
 	fortigate_ipsec_tunnel_up{name="some-network",p2serial="14",parent="My VPN",vdom="root"} 1
+	# HELP fortigate_ipsec_tunnels_total Total number of configured IPsec tunnels
+	# TYPE fortigate_ipsec_tunnels_total gauge
+	fortigate_ipsec_tunnels_total{vdom="root"} 1
+	# HELP fortigate_ipsec_tunnels_up Number of active IPsec tunnels
+	# TYPE fortigate_ipsec_tunnels_up gauge
+	fortigate_ipsec_tunnels_up{vdom="root"} 1
 	`
 
 	if err := testutil.GatherAndCompare(r, strings.NewReader(em)); err != nil {
